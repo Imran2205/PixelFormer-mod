9a25b6f92d634783a735cff91c09205936374748
imran2205 <imrankabir1996@gmail.com>
2023-01-04 11:58:09 +0600

    fine tune

--- pixelformer/dataloaders/dataloader.py
+++ pixelformer/dataloaders/dataloader.py
@@ -96,7 +96,7 @@ class DataLoadPreprocess(Dataset):
             image_path = os.path.join(self.args.data_path, rgb_file)
             depth_path = os.path.join(self.args.gt_path, depth_file)
     
-            image = Image.open(image_path)
+            image = Image.open(image_path).convert('RGB')
             depth_gt = Image.open(depth_path)
 
             if self.args.do_kb_crop is True:
@@ -156,7 +156,7 @@ class DataLoadPreprocess(Dataset):
                 data_path = self.args.data_path
 
             image_path = os.path.join(data_path, "./" + sample_path.split()[0])
-            image = np.asarray(Image.open(image_path), dtype=np.float32) / 255.0
+            image = np.asarray(Image.open(image_path).convert('RGB'), dtype=np.float32) / 255.0
 
             if self.mode == 'online_eval':
                 gt_path = self.args.gt_path_eval
